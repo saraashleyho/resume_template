22f1d28f25fc9ec586d7ef79b2ff3ba6473f0b0c
Ho <rbish005@BKUSL-1FP6P3XY.local>
2020-07-30 16:51:31 -0400

    start to populate resume with personal data

---
+++ resume.js
@@ -0,0 +1,32 @@
+const data = {
+  name: 'Sara Ho',
+  mainEmoji: {
+    character: '👋',
+    ariaLabel: 'wave',
+  },
+  phoneNumber: '978',
+  email: 'sho',
+  githubHandle: 'xxx',
+  linkedInHandle: 'yyy',
+};
+
+document.title = data.name;
+
+const name = document.querySelector('#name');
+name.textContent = data.name;
+
+const emojiContainer = document.querySelector('h1 span[role="img"]');
+emojiContainer.textContent = data.mainEmoji.character;
+emojiContainer.setAttribute('aria-label', data.mainEmoji.ariaLabel);
+
+const phoneNumberContainer = document.querySelector('#phone-number');
+phoneNumberContainer.textContent = data.phoneNumber;
+
+const emailContainer = document.querySelector('#email');
+emailContainer.textContent = data.email;
+
+const githubContainer = document.querySelector('#github-link');
+githubContainer.textContent += data.githubHandle;
+
+const linkedInContainer = document.querySelector('#linkedin-link');
+linkedInContainer.textContent += data.linkedInHandle;
